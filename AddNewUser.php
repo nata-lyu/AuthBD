5827952a6e30e4d95f1533997126d3f52613a9fb
Nata Lyubkina <nata_lyubkina@mail.ru>
2020-11-28 13:59:23 +0300

    newUser добавляется в БД через PDO

--- AddNewUser.php
+++ AddNewUser.php
@@ -1,10 +1,15 @@
 <?php
+require_once 'connectPDO.php';
 // добавляем данные нового польз-ля в БД
 $user = $_POST['username'];
 $pass = $_POST['password'];
+
 echo $user.".".$pass;
-$query = "INSERT INTO MyAuth (username, password) VALUES ('$user', '$pass')";
-if (mysqli_query($link, $query) == false) {
+$db->query("INSERT INTO MyAuth (username, password) VALUES ('$user', '$pass')");
+
+
+//$query = "INSERT INTO MyAuth (username, password) VALUES ('$user', '$pass')";
+/*if (mysqli_query($link, $query) == false) {
    die(mysqli_error($link));
-   }
+   }*/
 ?> 
